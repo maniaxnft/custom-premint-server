f30ef2f867648efbca6a4a6a23f2a750c0be772b
onuriltan <onur.iltan@gmail.com>
2021-11-25 13:29:39 +0300

    fix: word

--- src/components/body/jumbotron/Jumbotron.jsx
+++ src/components/body/jumbotron/Jumbotron.jsx
@@ -6,12 +6,12 @@ const Jumbotron = () => {
     <div className="jumbotron">
       <div className="jumbotron-content">
         <div className="jumbotron-title">
-          BRINGING ORIGINAL MANIAK NFTS TO THE
+          BRINGING ORIGINAL MANIAC NFTS TO THE
           <span className="jumbotron-title-sol"> SOLANA COMMUNITY</span>
         </div>
         <div className="jumbotron-explanation">
-          Solmaniaks is a collection of randomly created NFTs by our engineers
-          genetic algortihm. These 1111 Maniaks live and breathe in Solana blockchain.
+          Solmaniacs is a collection of randomly created NFTs by our engineers
+          genetic algortihm. These 1111 Maniacs live and breathe in Solana blockchain.
         </div>
       </div>
     </div>
