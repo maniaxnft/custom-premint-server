4dcd0bea75b00b69b52772fc9c180186ff6701ad
onuriltan <onur.iltan@gmail.com>
2021-12-06 13:40:50 +0300

    add wallet connection

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import "./Header.css";
+import {
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
 
-const Header = () => {
+import "./Header.css";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+const Header = ({
+  candyMachineId,
+  config,
+  connection,
+  startDate,
+  treasury,
+  txTimeout,
+}) => {
   return (
     <nav className="header">
       <div className="header-content">
-        <div className="header-logo unselectable"> SOLMANIACS </div>
-        <div className="header-connect-wallet-button unselectable">Connect Wallet</div>
+        <div className="header-logo unselectable"> SOLANA NFT </div>
+        <div className="header-wallets">
+          <WalletMultiButton className="header-wallets-connect-wallet" />
+          <WalletDisconnectButton className="header-wallets-disconnect-wallet" />
+        </div>
       </div>
     </nav>
   );
